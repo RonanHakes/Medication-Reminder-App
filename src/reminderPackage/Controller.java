30616f4d58ac1c0ef8cb5435f189d926e682b130
RonanHakes <ronan.hakes@gmail.com>
2021-07-01 20:49:35 -0400

    fixed first commit

--- src/reminderPackage/Controller.java
+++ src/reminderPackage/Controller.java
@@ -1,6 +1,7 @@
 package reminderPackage;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -12,4 +13,5 @@ public class Controller {
 
     public Label appTitle;
     public Text medicineNameHeader;
+    public Button confirmButton;
 }
